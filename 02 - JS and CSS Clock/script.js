61b3afc27b4746c87a7dea5105841d046bb90222
Aleksei Arro <polsck@gmail.com>
2019-12-14 13:42:58 +0300

    Clock work adjusted

--- 02 - JS and CSS Clock/script.js
+++ 02 - JS and CSS Clock/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const $secondHand = document.querySelector('.hand--second');
+const $minuteHand = document.querySelector('.hand--min');
+const $hourHand = document.querySelector('.hand--hour');
+
+const setHandPosition = (date, $hand, unitName, divisor) => {
+  const unit = date[`get${unitName}`]();
+  const degreesFromTime = (unit / divisor) * 360 + 90;
+  $hand.style.transform = `rotate(${degreesFromTime}deg)`;
+};
+
+const setDate = () => {
+  const now = new Date();
+
+  setHandPosition(now, $secondHand, 'Seconds', 60);
+  setHandPosition(now, $minuteHand, 'Minutes', 60);
+  setHandPosition(now, $hourHand, 'Hours', 12);
+};
+
+setInterval(setDate, 1000);
