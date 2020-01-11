9dc4b00381d7f04f1e2a47c6c44019c32c70b884
Aleksei Arro <polsck@gmail.com>
2020-01-12 00:04:58 +0300

    People list sorted by firstnames

--- 04 - Array Cardio Day 1/script.js
+++ 04 - Array Cardio Day 1/script.js
@@ -20,8 +20,6 @@ const inventors = [
   { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
 ];
 
-const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
-
 // Array.prototype.filter()
 // 1. Filter the list of inventors for those who were born in the 1500's
 
@@ -54,27 +52,88 @@ const getInventorYearsLived = (inventor) => (inventor.passed - inventor.year);
 
 const inventorsLifeDurationSum = inventors.reduce((sum, inventor) => {
   const age = getInventorYearsLived(inventor);
-  return sum += age;
+  return sum + age;
 }, 0);
 console.log(inventorsLifeDurationSum);
 
 // 5. Sort the inventors by years lived
 
-const inventorsSortedByYearsLived = [...inventors].sort((left, right) => {
-  const leftAge = getInventorYearsLived(left);
-  const rightAge = getInventorYearsLived(right);
-
-  return leftAge - rightAge;
-});
+const inventorsSortedByYearsLived = [...inventors]
+  .sort((left, right) => (getInventorYearsLived(left) - getInventorYearsLived(right)));
 console.log(inventorsSortedByYearsLived);
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
+const boulevards = [
+  `Boulevard Auguste-Blanqui`,
+  `Boulevard Barbès`,
+  `Boulevard Beaumarchais`,
+  `Boulevard de l'Amiral-Bruix`,
+  `Boulevard Mortier`,
+  `Boulevard Poniatowski`,
+  `Boulevard Soult`,
+  `Boulevard des Capucines`,
+  `Boulevard de la Chapelle`,
+  `Boulevard de Clichy`,
+  `Boulevard du Crime`,
+  `Boulevard du Général-d'Armée-Jean-Simon`,
+  `Boulevard Haussmann`,
+  `Boulevard de l'Hôpital`,
+  `Boulevard des Italiens`,
+  `Boulevard Lefebvre`,
+  `Boulevard de la Madeleine`,
+  `Boulevard de Magenta`,
+  `Boulevard Marguerite-de-Rochechouart`,
+  `Boulevard Montmartre`,
+  `Boulevard du Montparnasse`,
+  `Boulevard Raspail`,
+  `Boulevard Richard-Lenoir`,
+  `Boulevard Saint-Germain`,
+  `Boulevard Saint-Michel`,
+  `Boulevard de Sébastopol`,
+  `Boulevard de Strasbourg`,
+  `Boulevard du Temple`,
+  `Boulevard Voltaire`,
+  `Boulevard de la Zone`,
+];
+
+const filteredBoulevards = boulevards.filter((boulevard) => boulevard.includes('de'));
+console.log(filteredBoulevards);
 
 // 7. sort Exercise
-// Sort the people alphabetically by last name
+// Sort the people alphabetically by first name
+
+const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+
+const getPersonNameParts = (personName) => personName.split(/,\s*/);
+
+const peopleSortedByFirstname = [...people].sort((left, right) => {
+  const leftFirstname = getPersonNameParts(left)[1];
+  const rightFirstname = getPersonNameParts(right)[1];
+
+  if (leftFirstname < rightFirstname) {
+    return -1;
+  }
+
+  if (leftFirstname > rightFirstname) {
+    return 1;
+  }
+
+  return 0;
+});
+console.log(peopleSortedByFirstname);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
-const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+const carTypes = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+// const carTypesAmount = carTypes.reduce((result, type, i, arr) => {
+//   const
+// }, 0);
+
+const getCarTypesAmount = (typeList) => {
+  const typeSet = new Set(typeList);
+  return typeSet.size;
+};
+console.log(getCarTypesAmount(carTypes));
