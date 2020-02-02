ce014004594a525e367a29291d24d90ce9f4d081
Aleksei Arro <polsck@gmail.com>
2020-02-02 19:43:40 +0300

    Added regExp for number dot insertion function

--- 06 - AJAX Type Ahead/script.js
+++ 06 - AJAX Type Ahead/script.js
@@ -1,14 +1,19 @@
 'use strict';
 
 const ENDPOINT = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const CITY_OBJECT_FIELDS = ['city', 'state'];
 
 const cities = [];
 
 function findMatches(wordToMatch) {
   return cities.filter((place) => {
     const matcher = new RegExp(wordToMatch, 'gi');
-    return ['city', 'state'].some((it) => place[it].match(matcher));
+    return CITY_OBJECT_FIELDS.some((it) => place[it].match(matcher));
   });
+}
+
+function addCommasToNumString(num) {
+  return String(num).replace(/\B(?=(\d{3})+$)/g, '.');
 }
 
 function addSearchHandlers() {
@@ -20,13 +25,23 @@ function addSearchHandlers() {
   const $list = $form.querySelector('.suggestions');
   function handleMatch() {
     const matchedList = findMatches(this.value);
-    const markup = matchedList.reduce((acc, place) => (
-      `${acc}
-      <li>
-        <span class="name">${place.city}, ${place.state}</span>
-        <span class="population">${place.population}</span>
-      </li>`
-    ), '');
+
+    const markup = matchedList.reduce((acc, place) => {
+      const matcher = new RegExp(this.value, 'gi');
+      const [cityName, stateName] = CITY_OBJECT_FIELDS.map((str) => (
+        place[str].replace(matcher, `<span class="hl">${this.value}</span>`)
+      ));
+      const population = addCommasToNumString(place.population);
+
+      return (
+        `${acc}
+        <li>
+          <span class="name">${cityName}, ${stateName}</span>
+          <span class="population">${population}</span>
+        </li>`
+      );
+    }, '');
+
     $list.innerHTML = markup;
   }
 
