bec44f616abfaa616018372978f9fe559227e3ae
Aleksei Arro <polsck@gmail.com>
2020-02-02 16:42:49 +0300

    HTML generation from suggestion list added

--- 06 - AJAX Type Ahead/script.js
+++ 06 - AJAX Type Ahead/script.js
@@ -2,37 +2,47 @@
 
 const ENDPOINT = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-function findMatches(wordToMatch, listToFilter) {
-  return listToFilter.filter((place) => {
+const cities = [];
+
+function findMatches(wordToMatch) {
+  return cities.filter((place) => {
     const matcher = new RegExp(wordToMatch, 'gi');
     return ['city', 'state'].some((it) => place[it].match(matcher));
   });
 }
 
-function displayMatches(dataList) {
+function addSearchHandlers() {
   const $form = document.querySelector('.search-form');
-  const $input = $form.querySelector('.search');
-  const $list = $form.querySelector('.suggestions');
-
   $form.addEventListener('submit', (evt) => {
     evt.preventDefault();
   });
 
-  $input.addEventListener('change', function () {
-    const matchedList = findMatches(this.value, dataList);
-    console.log(matchedList);
-  });
+  const $list = $form.querySelector('.suggestions');
+  function handleMatch() {
+    const matchedList = findMatches(this.value);
+    const markup = matchedList.reduce((acc, place) => (
+      `${acc}
+      <li>
+        <span class="name">${place.city}, ${place.state}</span>
+        <span class="population">${place.population}</span>
+      </li>`
+    ), '');
+    $list.innerHTML = markup;
+  }
+
+  const $input = $form.querySelector('.search');
+  $input.addEventListener('change', handleMatch);
+  $input.addEventListener('input', handleMatch);
 }
 
 function fetchList() {
-  const cities = [];
   window.fetch(ENDPOINT)
     .then((response) => response.json())
     .then((list) => {
       cities.push(...list);
     })
     .then(() => {
-      displayMatches(cities);
+      addSearchHandlers();
     })
     .catch((err) => {
       console.warn(err.message);
