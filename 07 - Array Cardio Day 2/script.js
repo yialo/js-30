796f4e1c654d58682cfac10fd87fb327788e1d43
Aleksei Arro <polsck@gmail.com>
2020-08-09 18:54:31 +0300

    Add Array.prototype.some use-case

--- 07 - Array Cardio Day 2/script.js
+++ 07 - Array Cardio Day 2/script.js
@@ -1,12 +1,10 @@
-'use strict';
-
 // ## Array Cardio Day 2
 
 const people = [
   { name: 'Wes', year: 1988 },
   { name: 'Kait', year: 1986 },
   { name: 'Irv', year: 1970 },
-  { name: 'Lux', year: 2015 }
+  { name: 'Lux', year: 2015 },
 ];
 
 const comments = [
@@ -14,12 +12,28 @@ const comments = [
   { text: 'Super good', id: 823423 },
   { text: 'You are the best', id: 2039842 },
   { text: 'Ramen is my fav food ever', id: 123523 },
-  { text: 'Nice Nice Nice!', id: 542328 }
+  { text: 'Nice Nice Nice!', id: 542328 },
 ];
 
 // Some and Every Checks
-// Array.prototype.some() // is at least one person 19 or older?
-// Array.prototype.every() // is everyone 19 or older?
+// is at least one person 19 or older?
+const THRESHOLD_AGE = 19;
+
+const isAllNineteenOrOlder = people.some(({ year }) => {
+  const birthDate = new Date(year + 1) - 1;
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const lastMillisecondOfThisYear = new Date(currentYear + 1) - 1;
+
+  const isBirthdayGone = currentDate.getTime() === lastMillisecondOfThisYear;
+  const age = currentYear - year - (isBirthdayGone ? 0 : 1);
+
+  return age >= THRESHOLD_AGE;
+});
+console.log(isAllNineteenOrOlder);
+
+// is everyone 19 or older?
+// Array.prototype.every()
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
