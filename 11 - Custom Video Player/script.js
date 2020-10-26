a275d658a4c1152bc7748ced09a8b4c8facf64d8
Aleksei Arro <polsck@gmail.com>
2020-10-26 20:48:05 +0300

    Tune progressbar in

--- 11 - Custom Video Player/script.js
+++ 11 - Custom Video Player/script.js
@@ -2,13 +2,20 @@
 
 const $player = document.querySelector('.player');
 const $video = $player.querySelector('.viewer');
-const $progress = $player.querySelector('.progress');
-const $progressBar = $progress.querySelector('.progress__filled');
+const $progressBar = $player.querySelector('.progress');
+const $progressFilled = $progressBar.querySelector('.progress__filled');
 const $toggle = $player.querySelector('.toggle');
 const $skipButtons = $player.querySelectorAll('[data-skip]');
 const $ranges = $player.querySelectorAll('[type="range"]');
 
 // Build our handlers
+
+const setProgressPosition = (percentage) => {
+  $progressFilled.style.flexBasis = `${percentage}%`;
+  $progressBar.setAttribute('aria-valuenow', percentage);
+  const rounded = Math.round(percentage);
+  $progressBar.setAttribute('aria-valuetext', `Playback position: ${rounded}%`);
+};
 
 const togglePlay = () => {
   const method = $video.paused ? 'play' : 'pause';
@@ -22,16 +29,44 @@ const updateButton = () => {
 };
 
 const skip = (event) => {
-  const skipAmount = Number.parseInt(event.target.dataset.skip, 10);
+  const skipAmount = Number.parseFloat(event.target.dataset.skip);
   $video.currentTime += skipAmount;
+};
+
+const handleRangeUpdate = (event) => {
+  const $range = event.target;
+  $video[$range.name] = $range.value;
+};
+
+const handleProgress = () => {
+  const percentage = $video.currentTime / $video.duration * 100;
+  setProgressPosition(percentage);
+};
+
+const handleProgressBarClick = (event) => {
+  const factor = event.offsetX / $progressBar.offsetWidth;
+  console.log(factor);
+  $video.currentTime = $video.duration * factor;
+
+  const percentage = factor * 100;
+  setProgressPosition(percentage);
 };
 
 // Hook up the event listeners
 
+$progressBar.addEventListener('click', handleProgressBarClick);
+
+$ranges.forEach(($range) => {
+  $range.addEventListener('change', handleRangeUpdate);
+});
+
 $toggle.addEventListener('click', togglePlay);
-$video.addEventListener('click', togglePlay);
-$video.addEventListener('play', updateButton);
-$video.addEventListener('pause', updateButton);
+
 $skipButtons.forEach(($button) => {
   $button.addEventListener('click', skip);
 })
+
+$video.addEventListener('click', togglePlay);
+$video.addEventListener('play', updateButton);
+$video.addEventListener('pause', updateButton);
+$video.addEventListener('timeupdate', handleProgress);
