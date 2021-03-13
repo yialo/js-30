5c29c34fcccd9a8089912a9077ce2a91365699ee
Aleksei Arro <polsck@gmail.com>
2021-03-13 21:56:13 +0300

    Complete lesson 13

--- 13 - Slide in on Scroll/index.js
+++ 13 - Slide in on Scroll/index.js
@@ -1,14 +1,42 @@
-function debounce(func, wait = 20, immediate = true) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
+const debounce = (func, wait = 20, immediate = true) => {
+  let timeout;
+
+  return (...args) => {
+    const later = () => {
       timeout = null;
-      if (!immediate) func.apply(context, args);
+
+      if (!immediate) {
+        func(...args);
+      }
     };
-    var callNow = immediate && !timeout;
+
+    const callNow = immediate && !timeout;
+
     clearTimeout(timeout);
     timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
+
+    if (callNow) {
+      func(...args);
+    }
   };
-}
+};
+
+const $images = document.querySelectorAll('.slide-in');
+
+const checkSlide = debounce((event) => {
+  $images.forEach(($image) => {
+    const windowBottom = window.scrollY + window.innerHeight;
+    const imageBottom = $image.offsetTop + $image.offsetHeight;
+
+    const isHalfShown = windowBottom >= $image.offsetTop + $image.offsetHeight / 2;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      $image.classList.add('active');
+    } else {
+      $image.classList.remove('active');
+    }
+  });
+});
+
+window.addEventListener('scroll', checkSlide);
