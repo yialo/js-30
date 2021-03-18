4f60d187e0fcef59b06697b5ff3a85e2829217f1
Aleksei Arro <polsck@gmail.com>
2021-03-18 20:22:38 +0300

    Provide my own solution

--- 17 - Sort Without Articles/index.js
+++ 17 - Sort Without Articles/index.js
@@ -1,1 +1,24 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const BANDS = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const removeArticle = (str) => str.replace(/^(the|an?) /i, '');
+
+const sortWithoutArticles = (nameList) => nameList.sort((left, right) => {
+  const leftWithoutArticle = removeArticle(left);
+  const rightWithoutArticle = removeArticle(right);
+
+  if (leftWithoutArticle < rightWithoutArticle) {
+    return -1;
+  }
+
+  if (leftWithoutArticle > rightWithoutArticle) {
+    return 1;
+  }
+
+  return 0;
+});
+
+const init = () => {
+  console.log(sortWithoutArticles(BANDS));
+};
+
+init();
