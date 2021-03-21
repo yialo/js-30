dbd336838ae5faabca9e406b9dbe065a5d1fe329
Aleksei Arro <polsck@gmail.com>
2021-03-21 23:12:46 +0300

    Pass webcam stream to canvas

--- 19 - Webcam Fun/index.js
+++ 19 - Webcam Fun/index.js
@@ -1,5 +1,34 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const $video = document.querySelector('.player');
+const $canvas = document.querySelector('.photo');
+const $strip = document.querySelector('.strip');
+const $snap = document.querySelector('.snap');
+
+const ctx = $canvas.getContext('2d');
+
+const paintToCanvas = () => {
+  const { videoWidth: width, videoHeight: height } = $video;
+
+  $canvas.width = width;
+  $canvas.height = height;
+
+  const paintFrame = () => {
+    ctx.drawImage($video, 0, 0, width, height);
+    window.requestAnimationFrame(paintFrame);
+  };
+
+  window.requestAnimationFrame(paintFrame);
+};
+
+const getVideo = () => {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then((localMediaStream) => {
+    $video.srcObject = localMediaStream;
+
+    $video.play().then(() => {
+      paintToCanvas();
+    });
+  }).catch((error) => {
+    console.error('Oh no!', error);
+  })
+};
+
+getVideo();
