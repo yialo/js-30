8d65dbe41f7ecc78056afc685c73d7084c2fe280
Aleksei Arro <polsck@gmail.com>
2021-03-22 00:01:33 +0300

    Add ability to take photos

--- 19 - Webcam Fun/index.js
+++ 19 - Webcam Fun/index.js
@@ -1,7 +1,8 @@
-const $video = document.querySelector('.player');
 const $canvas = document.querySelector('.photo');
-const $strip = document.querySelector('.strip');
+const $shutter = document.querySelector('.shutter');
 const $snap = document.querySelector('.snap');
+const $strip = document.querySelector('.strip');
+const $video = document.querySelector('.player');
 
 const ctx = $canvas.getContext('2d');
 
@@ -11,12 +12,18 @@ const paintToCanvas = () => {
   $canvas.width = width;
   $canvas.height = height;
 
+  const requestIdRef = {
+    value: null,
+  };
+
   const paintFrame = () => {
     ctx.drawImage($video, 0, 0, width, height);
-    window.requestAnimationFrame(paintFrame);
+    requestIdRef.value = window.requestAnimationFrame(paintFrame);
   };
 
   window.requestAnimationFrame(paintFrame);
+
+  return requestIdRef;
 };
 
 const getVideo = () => {
@@ -24,11 +31,38 @@ const getVideo = () => {
     $video.srcObject = localMediaStream;
 
     $video.play().then(() => {
-      paintToCanvas();
+      const requestIdRef = paintToCanvas();
     });
   }).catch((error) => {
     console.error('Oh no!', error);
   })
 };
+
+const takePhoto = () => {
+  $snap.currentTime = 0;
+  $snap.play();
+
+  const dataFromCanvas = $canvas.toDataURL('image/jpeg');
+  const $link = document.createElement('a');
+  $link.href = dataFromCanvas;
+  $link.setAttribute('download', 'photo');
+  $link.setAttribute('aria-label', 'Download image');
+
+  const $img = document.createElement('img');
+  $img.src = dataFromCanvas;
+  $img.alt = 'It\'s me';
+
+  $link.append($img);
+
+  $strip.prepend($link);
+
+  if ($strip.childElementCount === 0) {
+    $strip.classList.remove('strip_with-padding');
+  } else {
+    $strip.classList.add('strip_with-padding');
+  }
+};
+
+$shutter.addEventListener('click', takePhoto);
 
 getVideo();
