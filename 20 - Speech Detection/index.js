74b4c73a33924ed1460df4e22042b4bf6044d9bb
Aleksei Arro <polsck@gmail.com>
2021-03-22 23:51:38 +0300

    Prepare basic speech recognition

--- 20 - Speech Detection/index.js
+++ 20 - Speech Detection/index.js
@@ -1,1 +1,14 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition ?? window.webkitSpeechRecognition;
+
+const recognition = new window.SpeechRecognition();
+recognition.interimResults = true;
+
+let $paragraph = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild($paragraph);
+
+recognition.addEventListener('result', (event) => {
+  console.log(event);
+});
+
+recognition.start();
