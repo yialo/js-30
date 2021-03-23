0938bba739592c332d1787a1ecd6c7468d5c2454
Aleksei Arro <polsck@gmail.com>
2021-03-23 15:02:48 +0300

    Complete lesson 20

--- 20 - Speech Detection/index.js
+++ 20 - Speech Detection/index.js
@@ -1,14 +1,20 @@
 window.SpeechRecognition = window.SpeechRecognition ?? window.webkitSpeechRecognition;
 
 const recognition = new window.SpeechRecognition();
-recognition.interimResults = true;
 
-let $paragraph = document.createElement('p');
-const words = document.querySelector('.words');
-words.appendChild($paragraph);
+const $words = document.querySelector('.words');
+
+const addParagraph = (text) => {
+  let $paragraph = document.createElement('p');
+  $paragraph.textContent = text;
+  $words.appendChild($paragraph);
+};
 
 recognition.addEventListener('result', (event) => {
-  console.log(event);
+  const phase = Array.from(event.results[0], ({ transcript }) => transcript ).join(' ');
+  addParagraph(phase);
 });
+
+recognition.addEventListener('end', recognition.start);
 
 recognition.start();
