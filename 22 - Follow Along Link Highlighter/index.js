fd635a01c3e59b07e6cf53247e96861420c7824f
Aleksei Arro <polsck@gmail.com>
2021-03-25 16:02:43 +0300

    Complete lesson 22

--- 22 - Follow Along Link Highlighter/index.js
+++ 22 - Follow Along Link Highlighter/index.js
@@ -1,1 +1,19 @@
-// 👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀
+const list$link = document.querySelectorAll('a');
+
+const $highlight = document.createElement('span');
+$highlight.classList.add('highlight');
+document.body.append($highlight);
+
+const  highlightLink = ({ target }) => {
+  const { left, top, width, height } = target.getBoundingClientRect();
+
+  $highlight.style.left = `${window.scrollX + left}px`;
+  $highlight.style.top = `${window.scrollY + top}px`;
+
+  $highlight.style.width = `${width}px`;
+  $highlight.style.height = `${height}px`;
+};
+
+list$link.forEach(($link) => {
+  $link.addEventListener('mouseenter', highlightLink);
+});
