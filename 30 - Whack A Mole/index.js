8d05a9c6d2255eafe95bdefc29256ffc2bdfdead
Aleksei Arro <polsck@gmail.com>
2021-04-06 23:16:12 +0300

    Scaffold lesson 30

---
+++ 30 - Whack A Mole/index.js
@@ -0,0 +1,4 @@
+const $holes = document.querySelectorAll('.hole');
+const $moles = document.querySelectorAll('.mole');
+const $scoreBoard = document.querySelector('.score');
+const $startButton = document.querySelector('.start-button');
