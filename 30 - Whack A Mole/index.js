cb92efb2173ae7db1e3f3805e7a6862c990c5ff9
Aleksei Arro <polsck@gmail.com>
2021-04-07 13:38:26 +0300

    Complete lesson 30

--- 30 - Whack A Mole/index.js
+++ 30 - Whack A Mole/index.js
@@ -1,13 +1,10 @@
 const $holes = document.querySelectorAll('.hole');
-const $moles = document.querySelectorAll('.mole');
 const $scoreBoard = document.querySelector('.score');
 const $startButton = document.querySelector('.start-button');
 
 const GAME_DURATION = 7000;
-const MIN_DURATION = 300;
-const MAX_DURATION = 1000;
-
-const getRandomHoleIndex = () => Math.floor(Math.random() * $holes.length);
+const MIN_MOLE_DURATION = 200;
+const MAX_MOLE_DURATION = 1000;
 
 let lastHoleIndex;
 
@@ -15,7 +12,7 @@ const getRandomHole = () => {
   let holeIndex;
 
   do {
-    holeIndex = getRandomHoleIndex();
+    holeIndex = Math.floor(Math.random() * $holes.length);
   } while (holeIndex === lastHoleIndex);
 
   lastHoleIndex = holeIndex;
@@ -28,21 +25,48 @@ const getRandomTime = (min, max) => Math.floor(min + Math.random() * (max - min 
 let gameEndTime;
 
 const showMole = () => {
-  if (Date.now() < gameEndTime) {
-    const duration = getRandomTime(MIN_DURATION, MAX_DURATION);
-    const $hole = getRandomHole();
-    $hole.classList.add('up');
+  const duration = getRandomTime(MIN_MOLE_DURATION, MAX_MOLE_DURATION);
 
-    window.setTimeout(() => {
-      $hole.classList.remove('up');
-      showMole();
-    }, duration);
+  const $hole = getRandomHole();
+  $hole.classList.add('up');
+
+  const $mole = $hole.querySelector('.mole');
+
+  const hideMole = () => {
+    $mole.removeEventListener('click', scoreUp, { once: true });
+    $hole.classList.remove('up');
+  };
+
+  const scoreUp = (event) => {
+  if (!event.isTrusted) {
+    return;
   }
+
+    const currentScore = +$scoreBoard.textContent;
+    $scoreBoard.textContent = currentScore + 1;
+    hideMole();
+  };
+
+  $mole.addEventListener('click', scoreUp, { once: true });
+
+  window.setTimeout(() => {
+    if ($hole.classList.contains('up')) {
+      hideMole();
+    }
+
+    if (Date.now() < gameEndTime) {
+      showMole();
+    } else {
+      $startButton.disabled = false;
+    }
+  }, duration);
 };
 
 const startGame = () => {
+  $scoreBoard.textContent = 0;
   gameEndTime = Date.now() + GAME_DURATION;
   showMole();
+  $startButton.disabled = true;
 };
 
 $startButton.addEventListener('click', startGame);
